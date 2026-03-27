b01037cdcbf165a4d5e8c6c5f7cf82766f10efbe
Dynamicphavy <favourajayi308@gmail.com>
2026-03-27 10:35:01 +0100

    web

--- health_vital/secrets.h
+++ health_vital/secrets.h
@@ -5,12 +5,9 @@
 #define WIFI_SSID "MTN_4G_329961"
 #define WIFI_PASSWORD "Phave1055@"
 
-#define BLYNK_TEMPLATE_ID "TMPL2hj3X21yt"
-#define BLYNK_TEMPLATE_NAME "Health Monitoring"
-#define BLYNK_AUTH_TOKEN "ntwzQwBv-3k4_pJdK5cFbiQ14YOdUl0w"
-
-#define Web_API_KEY ""
-#define DATABASE_URL ""
+// Database credentials
+#define Web_API_KEY "AIzaSyDM9C82uPDtmPktWPE7yOFnFHBkVrXcgJs"
+#define DATABASE_URL "https://wearable-health-device-default-rtdb.firebaseio.com/"
 #define USER_EMAIL ""
 #define USER_PASS ""
 
